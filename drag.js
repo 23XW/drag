35b8a1ac078115895228ed55105b2aed5f1db51f
23XW <275864454@aqq.com>
2017-09-06 21:47:33 +0800

    change5 drag.js

--- drag.js
+++ drag.js
@@ -2,7 +2,11 @@ function drag(id){
     var obj=document.getElementById(id);t
     var a=0;
     var b=0;
-    obj.onmousedown=function () {
+    obj.onmousedown=function (ev) {
+
+        disX=ev.pageX-obj.offsetLeft;
+        disY=ev.pageY-obj.offsetLeft;
+        
         
     }
 
