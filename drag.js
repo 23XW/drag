bb4d5a37566a6fd63f90bc6b514cd0840824a3fc
23XW <275864454@aqq.com>
2017-09-06 22:04:47 +0800

    change6 drag.js from master

--- drag.js
+++ drag.js
@@ -6,7 +6,11 @@ function drag(id){
 
         disX=ev.pageX-obj.offsetLeft;
         disY=ev.pageY-obj.offsetLeft;
-        
+        document.onmousemove=function (ev) {
+            obj.style.left=ev.pageX-disX+'PX';
+            obj.style.top=ev.pageY-disY+'PX';
+
+        }
         
     }
 
