47db8f6ef4180ec4014d1c03f6f1fa9f5b0fe847
23XW <275864454@aqq.com>
2017-09-06 23:19:47 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,17 +1,21 @@
 function drag(id){
-    var obj=document.getElementById(id);t
-    var a=0;
-    var b=0;
-    obj.onmousedown=function (ev) {
+    var obj=document.getElementById(id);
+    var disX=0;
+    var disY=0;
+    obj.onmousedown =function (ev) {
 
         disX=ev.pageX-obj.offsetLeft;
-        disY=ev.pageY-obj.offsetLeft;
+        disY=ev.pageY-obj.offsetTop;
 
-        document.onmousemove=function () {
-            obj.style.left=ev.clientX-disX+"PX";
-            obj.style.left=ev.clientY-disY+"PX";
+        document.onmousemove = function (ev) {
+            obj.style.left=ev.pageX-disX+"PX";
+            obj.style.top=ev.pageY-disY+"PX";
+        };
+        document.onmousmove=function () {
+            document.onmousemove=null;
+            document.onmouseup=null;
+
         }
-        
     }
-
+return false;
 }
